dfdcb589ca58cd8e196e2117b4a2065047f0422e
Goutam Patidar <73788341+GoutamPatidar@users.noreply.github.com>
2024-05-12 04:04:58 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,21 @@
+//iife
+
+(function chai(){
+    console.log(`DB connected`);
+})();
+//-> named iife
+//; ->is used to addnew iife function 
+
+//first ()-> defination of fn and 2nd () is execution of function
+
+//why iife?
+
+//iife are the function which execute imediately and sometime Global varibale pollute the function call and to avoid thatpoluttion we use iife 
+
+(()=>{
+    console.log(`DB connected`);
+})();
+
+((name)=>{
+    console.log(`DB connected ${name}`);
+})("goutam");

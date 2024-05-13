9d25cf56f8dd883a175225473f1e184ca25c62b1
Goutam Patidar <73788341+GoutamPatidar@users.noreply.github.com>
2024-05-13 05:26:56 +0000

    loops

---
+++ 05_loops_iterations/one.js
@@ -0,0 +1,19 @@
+//for loop
+
+let arr=[1,2,5,6,4,87,98]
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element);
+    
+}
+
+/*
+for()
+    {
+        for()
+            {
+
+            }
+    }
+
+*/
---
+++ 05_loops_iterations/two.js
@@ -0,0 +1,14 @@
+//while loop
+
+
+let i=0;
+while (i<=10) {
+    console.log(`i value is ${i++} `);
+}
+
+//dowhile loop
+
+i=0;
+do {
+    console.log(`i value is ${i++} `);
+}while (i<=10);

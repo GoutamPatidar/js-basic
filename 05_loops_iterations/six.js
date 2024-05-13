b0de382b3a1a0970e9b74a45ffb7dcc63f592104
Goutam Patidar <73788341+GoutamPatidar@users.noreply.github.com>
2024-05-13 16:31:27 +0000

    filter

---
+++ 05_loops_iterations/six.js
@@ -0,0 +1,33 @@
+// const coding=["js","ruby","java","cpp"]
+
+// const val=coding.forEach((i)=>{
+//     console.log(i);
+//     return i;
+// })
+
+// console.log(val);
+
+
+
+
+// const val=coding.forEach((i)=>{
+//     console.log(i);
+//     return i;
+// })
+
+
+// console.log(val);
+
+const num=[1,2,3,4,5,6,7,8,9,10]
+
+// const newnum=num.filter((num)=> num>4)
+
+// console.log(newnum);
+
+
+const newnum=num.filter((num)=>{
+     return num>4
+    
+    })
+
+console.log(newnum);

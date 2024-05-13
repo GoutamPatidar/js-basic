00d8ba4089466fe87bf211a1caa82d96f0c722c8
Goutam Patidar <73788341+GoutamPatidar@users.noreply.github.com>
2024-05-13 06:50:39 +0000

    objects,maps and iteration on them

---
+++ 05_loops_iterations/three_four.js
@@ -0,0 +1,69 @@
+//for of
+
+
+// [",",""]
+// [{},{},{}]
+
+
+// const arr=[1,2,5,6,4,7,8,9]
+
+const arr="goutampatidar"
+
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+//maps
+
+const m=new Map()
+m.set('a',1)
+m.set('b',2)
+m.set('c',1)
+m.set('d',3)
+
+// console.log(m);
+
+
+// console.log(m.get('d'));
+// console.log(m.get('e'));
+
+// for (const [key,val] of m) {
+//     console.log(key + val);
+// }
+
+
+/********************************************************************* */
+
+const obj={
+    'g':1,
+    'g2':2
+}
+
+// for (const [key,val] of obj) {
+//     console.log(key + val);
+// }// we cannot iterate over object using this for of loop 
+
+
+/********************************************************************* */
+//for in loop in  object
+
+// const objj={
+//     'g':1,
+//     'g2':2
+// }
+
+// for (const key in obj) {
+//     console.log(obj[key] );
+// }
+
+
+/************************************************************************** */
+
+// const ar=[1,2,5,6,4,7,8,9]
+// for (const key in ar) {
+//     console.log(ar[key]);
+// }
+
+
+
+//maps is not iteratable and all other are iteratable 
